a97d6c45511db0257cd0b63264deef2c049cfe4e
JV <ch.jesus.99@gmail.com>
2025-04-13 01:00:16 -0600

    se estilizan los reviews y se agrega script de leer mas en las mismas, tambien se modifica el degradado del header

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+let hideText_btn = document.getElementById('hideText_btn');
+
+let hideText = document.getElementById('hideText');
+
+hideText_btn.addEventListener('click', toggleText);
+
+function toggleText() {
+    hideText.classList.toggle('show');
+}
